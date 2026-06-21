33c150b2fd08f715715f0e65fa6ed9e0d55c511c
ceci217 <cecilia0799@gmail.com>
2026-06-21 10:15:55 +0200

    Delete compare.py

--- compare.py
+++
@@ -1,7 +0,0 @@
-def compare(old, new):
-    diff = {
-        "nuovi_obblighi": list(set(new["obblighi"]) - set(old["obblighi"])),
-        "nuove_scadenze": list(set(new["scadenze"]) - set(old["scadenze"])),
-        "modifiche_sanzioni": list(set(new["sanzioni"]) - set(old["sanzioni"]))
-    }
-    return diff
